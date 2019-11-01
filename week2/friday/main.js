5b1e981a8eb7c9a4206c428761ac8ca946fdee47
Stephan Elondo <selondo@nyc.yearup.org>
2019-11-01 17:01:18 -0400

    updated

--- week2/friday/main.js
+++ week2/friday/main.js
@@ -14,6 +14,12 @@
 // let helloWorld = () => {
 //   return 'helloWorld'
 // }
+
+let createButton = document.getElementById('create-button')
+let color1 = green
+let color2 = red
+let generateGrid = (color1, color2) =>{
+
 let rows = prompt('how many rows')
 let cols = prompt('how many columns')
 
@@ -28,9 +34,9 @@ let i = 0;
 while (i < (divCount)) {
   let div = document.createElement('div')
   if (i% 2 == 0) {
-    div.style.background = '#ccc'
+    div.style.background = color1
   } else {
-    div.style.background = '#333'
+    div.style.background = color2
   }
   grid.appendChild(div)
   i++
@@ -46,3 +52,6 @@ while (i < (divCount)) {
 
 grid.style.gridTemplate = `repeat(${rows}, 150px) / repeat(${cols}, 150px)`
 grid.style.gridGap = '3px'
+}
+
+createButton.addEventListener('click', () => generateGrid(color1,color2))
