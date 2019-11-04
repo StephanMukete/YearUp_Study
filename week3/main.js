cfd11aeed8fd90e04e738bb55760e1cb95b85175
Stephan Elondo <selondo@nyc.yearup.org>
2019-11-04 16:44:31 -0500

    animations with ball on a street[C

--- week3/main.js
+++ week3/main.js
@@ -53,22 +53,42 @@
 // but2.addEventListener('click', () => {document.body.style.backgroundColor = 'red'
 // })
 
-
-
 let playBtn = document.getElementById('play-btn')
+
 let pauseBtn = document.getElementById('pause-btn')
+
 let circle = document.getElementById('circle')
+
+let street = document.getElementById('street')
+
+let lines = street.children
 
 
 
 playBtn.addEventListener('click', () => {
-    circle.setAttribute('class', 'move')
-    circle.style.webkitAnimationPlayState = 'running'
-    setTimeout(() => {
+
+  circle.setAttribute('class', 'move')
+
+  circle.style.webkitAnimationPlayState = 'running'
+
+  setTimeout(() => {
+
     circle.classList.remove('move')
-  }, 12000)
+
+  }, 6000)
+
+
+
+  street.setAttribute('class', 'move')
+
+  street.style.animationDuration = 'infinite'
+
 })
 
+
+
 pauseBtn.addEventListener('click', () => {
+
   circle.style.webkitAnimationPlayState = 'paused'
+
 })
