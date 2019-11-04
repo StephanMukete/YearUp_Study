fe5471a762755d4f56b517d15e690b7dde7a55f9
Stephan Elondo <selondo@nyc.yearup.org>
2019-11-04 16:08:01 -0500

    play pause webkit

--- week3/main.js
+++ week3/main.js
@@ -1,10 +1,74 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let main = document.getElementById('main')
+// document.addEventListener('DOMContentLoaded', () => {
+//   let main = document.getElementById('main')
+//
+//   main.addEventListener('click', () => {
+//     console.log('successful');
+//   })
+// })
+// let count = 0
+// count++
+// setInterval(() => {
+//   count += 2
+//   console.log('changed bro');
+// }, 2000)
+// console.log(count);
+// let container = document.getElementById('btn-container')
+// // console.log(container.children);
+//
+// let btn = document.createElement('button')
+// btn.innerText = 'Change to green'
+// container.appendChild(btn)
+// // console.log(container.children);
+//
+// for (b of container.children){
+//   b.addEventListener('click', () => {
+//     // document.body.style.backgroundColor = 'green'
+//   })
+//   console.log(b);
+// }
+// let count = 0
+// let but1 = document.getElementById('btn1')
+// let but2 = document.getElementById('btn2')
+// but1.addEventListener('click', () => {
+//
+//    // document.body.style.backgroundColor = 'black'
+//   setInterval(() => {
+//     count += 1
+//     if (count === 1) {
+//       document.body.classList.add('animation')
+//       // document.body.style.backgroundColor = 'blue'
+//     }
+//     else if (count > 2 ) {
+//       document.body.classList.add('animation')
+//       document.body.style.backgroundColor = 'purple'
+//     }
+//     else{
+//       document.body.classList.add('animation')
+//       document.body.style.backgroundColor = 'pink'
+//     }
+//   }, 2000)
+// // document.body.style.
+//
+// })
+// but2.addEventListener('click', () => {document.body.style.backgroundColor = 'red'
+// })
 
-  main.addEventListener('click', () => {
-    console.log('successful');
-  })
+
+
+let playBtn = document.getElementById('play-btn')
+let pauseBtn = document.getElementById('pause-btn')
+let circle = document.getElementById('circle')
+
+
+
+playBtn.addEventListener('click', () => {
+    circle.setAttribute('class', 'move')
+    circle.style.webkitAnimationPlayState = 'running'
+    setTimeout(() => {
+    circle.classList.remove('move')
+  }, 12000)
 })
-setTimeout(() => {
-  main.remove()
-}, 5000)
+
+pauseBtn.addEventListener('click', () => {
+  circle.style.webkitAnimationPlayState = 'paused'
+})
